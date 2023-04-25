a131593dd2e377ed5e201b91eeb4b0dca39d1b09
AdiMunawar31 <munawar.adi31@gmail.com>
2023-04-25 15:50:17 +0700

    initialize project

---
+++ src/main/java/com/d2y/d2yreddit/D2yredditApplication.java
@@ -0,0 +1,13 @@
+package com.d2y.d2yreddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2yredditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2yredditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d2y/d2yreddit/D2yredditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d2y.d2yreddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2yredditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
