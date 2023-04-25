61fd055d89cd4c2514dd97096022b45aecb3a2b5
AdiMunawar31 <munawar.adi31@gmail.com>
2023-04-25 17:54:16 +0700

    delete annotation Builder at Post model

--- src/main/java/com/d2y/d2yreddit/models/Post.java
+++ src/main/java/com/d2y/d2yreddit/models/Post.java
@@ -12,13 +12,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Post {
