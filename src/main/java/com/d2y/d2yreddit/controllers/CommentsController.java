7bebfe7a129388be4eaa2c42324e5f71c3e64a64
AdiMunawar31 <munawar.adi31@gmail.com>
2023-04-27 19:26:18 +0700

    create comments post feature

---
+++ src/main/java/com/d2y/d2yreddit/controllers/CommentsController.java
@@ -0,0 +1,45 @@
+package com.d2y.d2yreddit.controllers;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.d2y.d2yreddit.dto.CommentsDto;
+import com.d2y.d2yreddit.services.CommentService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/comments")
+@RequiredArgsConstructor
+public class CommentsController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(CREATED);
+    }
+
+    @GetMapping(params = "postId")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@RequestParam Long postId) {
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForPost(postId));
+    }
+
+    @GetMapping(params = "userName")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@RequestParam String userName) {
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForUser(userName));
+    }
+
+}
